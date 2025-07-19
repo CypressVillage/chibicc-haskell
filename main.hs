522aee1927b82025bbda155c11872339e15090fa
cypress <2169626027@qq.com>
2025-07-20 03:00:06 +0800

    Add + and - operators

--- main.hs
+++ main.hs
@@ -8,5 +8,19 @@ parse []    = return ""
 parse [x]   = do
     putStrLn "  .globl main"
     putStrLn "main:"
-    putStrLn $ "  mov $" ++ x ++ ", %rax"
+    putStrLn $ "  mov $" ++ parseNum x ++ ", %rax"
+    putStr   $ parseOperator x
     return   "  ret"
+
+parseOperator :: String -> String
+parseOperator []   = ""
+parseOperator (x:xs)
+    | x == '+'  = "  add $" ++ parseNum xs ++ ", %rax\n" ++ parseOperator xs
+    | x == '-'  = "  sub $" ++ parseNum xs ++ ", %rax\n" ++ parseOperator xs
+    | otherwise = parseOperator xs
+
+parseNum :: String -> String
+parseNum [] = ""
+parseNum (x:xs)
+    | x `elem` ['0'..'9'] = x : parseNum xs
+    | otherwise           = ""
