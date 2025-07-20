9d19c1d3d75a8e44aecf904007a8abe127f60104
cypress <2169626027@qq.com>
2025-07-21 02:04:49 +0800

    Improve error message
    Now, chibicc can print out an error message with an error location
    like this:
    
      $ ./chibicc 1+foo
      1+foo
        ^ invalid token

--- main.hs
+++ main.hs
@@ -1,3 +1,4 @@
+import System.IO ( hPutStr, stderr )
 import System.Environment ( getArgs )
 import Data.Char ( isDigit )
 
@@ -6,38 +7,80 @@ data Token = TK_PUNCT String
            | TK_EOF
            deriving (Show)
 
+data Position = Position
+    { startCol :: Int
+    , endCol   :: Int
+    } deriving (Show)
+
+data LocatedToken = LocatedToken
+    { token    :: Token
+    , position :: Position
+    } deriving (Show)
+
+data CompilerError = LexError String String Position
+                   | ParseError String String Position
+                   deriving (Show)
+
 main :: IO ()
-main = getArgs >>= parse >>= putStr
+main = getArgs >>= parse >>= either handleError putStr
+    where
+        handleError (LexError msg expr pos) =
+            hPutStr stderr $ formatError msg expr pos
+        handleError (ParseError msg expr pos) =
+            hPutStr stderr $ formatError msg expr pos
 
-parse :: [String] -> IO String
-parse []    = return ""
-parse [x]   = do
-    putStrLn "  .globl main"
-    putStrLn "main:"
-    return $ (parseToken . tokenize) x
+        formatError msg expr pos = unlines
+            [ expr
+            , replicate (startCol pos - 1) ' ' ++ "^ " ++ msg
+            ]
 
-parseOperator :: Token -> Token -> String
-parseOperator (TK_PUNCT x) (TK_NUM n)
-    | x == "+"  = "  add $" ++ num ++ ", %rax\n"
-    | x == "-"  = "  sub $" ++ num ++ ", %rax\n"
+parse :: [String] -> IO (Either CompilerError String)
+parse []    = return $ Right ""
+parse (x:_) = pure $ do
+    tokens <- tokenize x
+    asm    <- parseToken x tokens
+    return $ unlines
+        [ "  .globl main"
+        , "main:"
+        , asm
+        , "  ret"
+        ]
+
+parseOperator :: String -> Int -> String
+parseOperator p n
+    | p == "+"  = "  add $" ++ num ++ ", %rax\n"
+    | p == "-"  = "  sub $" ++ num ++ ", %rax\n"
     where num = show n
 
-parseNum :: Token -> String
-parseNum (TK_NUM n) = "  mov $" ++ num ++ ", %rax\n"
-    where num = show n
+parseNum :: Int -> String
+parseNum n = "  mov $" ++ show n ++ ", %rax\n"
 
-tokenize :: String -> [Token]
-tokenize []                 = [TK_EOF]
-tokenize s@(x:xs)
-    | x == ' '              = tokenize xs
-    | x `elem` punctuators  = TK_PUNCT [x] : tokenize xs
-    | isDigit x             = TK_NUM (read num) : tokenize other
-    where punctuators = "+-"
-          (num, other) = span isDigit s
+tokenize :: String -> Either CompilerError [LocatedToken]
+tokenize input = tokenize' 1 input where
+    tokenize' :: Int -> String -> Either CompilerError [LocatedToken]
+    tokenize' col []             = Right [LocatedToken TK_EOF (Position col col)]
+    tokenize' col s@(x:xs)
+        | x == ' '              = tokenize' (col+1) xs
+        | x `elem` punctuators  =
+            let pos = Position col col
+            in (LocatedToken (TK_PUNCT [x]) pos : ) <$> tokenize' (col+1) xs
+        | isDigit x             =
+            let pos = Position col (col+len-1)
+            in (LocatedToken (TK_NUM (read num)) pos : ) <$> tokenize' (col+len) other
+        | otherwise             =
+            let pos = Position col col
+            in Left $ LexError "invalid token" input pos
+        where punctuators  = "+-"
+              (num, other) = span isDigit s
+              len          = length num
 
-parseToken :: [Token] -> String
-parseToken []       = ""
-parseToken s@(x:xs) =
-    case x of TK_PUNCT _ -> parseOperator x (head xs) ++ parseToken (tail xs)
-              TK_NUM _   -> parseNum x ++ parseToken xs
-              TK_EOF     -> "  ret\n"
+parseToken :: String -> [LocatedToken] -> Either CompilerError String
+parseToken expr []                           = Right ""
+parseToken expr (LocatedToken (TK_PUNCT p) _   : LocatedToken (TK_NUM n) _   : xs) =
+    (parseOperator p n ++) <$> parseToken expr xs
+parseToken expr (LocatedToken (TK_PUNCT _) _   : LocatedToken  _         pos : xs) =
+    Left $ ParseError "expected a number" expr pos
+parseToken expr (LocatedToken (TK_NUM   n) _   : xs                              ) =
+    (parseNum n ++) <$> parseToken expr xs
+parseToken expr (LocatedToken  TK_EOF      _   : xs                              ) =
+    Right ""
