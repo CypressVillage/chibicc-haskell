a4a6ab33d03420c90f253a46bda7e893b31a8fa4
cypress <2169626027@qq.com>
2025-07-20 13:32:45 +0800

    Add a tokenizer to allow space characters between tokens

--- main.hs
+++ main.hs
@@ -1,26 +1,43 @@
-import System.Environment ( getArgs )   
+import System.Environment ( getArgs )
+import Data.Char ( isDigit )
+
+data Token = TK_PUNCT String
+           | TK_NUM Int
+           | TK_EOF
+           deriving (Show)
 
 main :: IO ()
-main = getArgs >>= parse >>= putStrLn
+main = getArgs >>= parse >>= putStr
 
 parse :: [String] -> IO String
 parse []    = return ""
 parse [x]   = do
     putStrLn "  .globl main"
     putStrLn "main:"
-    putStrLn $ "  mov $" ++ parseNum x ++ ", %rax"
-    putStr   $ parseOperator x
-    return   "  ret"
+    return $ (parseToken . tokenize) x
 
-parseOperator :: String -> String
-parseOperator []   = ""
-parseOperator (x:xs)
-    | x == '+'  = "  add $" ++ parseNum xs ++ ", %rax\n" ++ parseOperator xs
-    | x == '-'  = "  sub $" ++ parseNum xs ++ ", %rax\n" ++ parseOperator xs
-    | otherwise = parseOperator xs
+parseOperator :: Token -> Token -> String
+parseOperator (TK_PUNCT x) (TK_NUM n)
+    | x == "+"  = "  add $" ++ num ++ ", %rax\n"
+    | x == "-"  = "  sub $" ++ num ++ ", %rax\n"
+    where num = show n
 
-parseNum :: String -> String
-parseNum [] = ""
-parseNum (x:xs)
-    | x `elem` ['0'..'9'] = x : parseNum xs
-    | otherwise           = ""
+parseNum :: Token -> String
+parseNum (TK_NUM n) = "  mov $" ++ num ++ ", %rax\n"
+    where num = show n
+
+tokenize :: String -> [Token]
+tokenize []                 = [TK_EOF]
+tokenize s@(x:xs)
+    | x == ' '              = tokenize xs
+    | x `elem` punctuators  = TK_PUNCT [x] : tokenize xs
+    | isDigit x             = TK_NUM (read num) : tokenize other
+    where punctuators = "+-"
+          (num, other) = span isDigit s
+
+parseToken :: [Token] -> String
+parseToken []       = ""
+parseToken s@(x:xs) =
+    case x of TK_PUNCT _ -> parseOperator x (head xs) ++ parseToken (tail xs)
+              TK_NUM _   -> parseNum x ++ parseToken xs
+              TK_EOF     -> "  ret\n"
